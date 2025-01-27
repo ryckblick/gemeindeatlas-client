c550b4aedd756341536c4307f54b1e037e95c0be
Eric <15267740+ryckblick@users.noreply.github.com>
2025-01-27 11:46:11 +0100

    implemented filters for /municipalities, added /all/regionalkeys endpoint

--- src/GemeindeatlasClient.php
+++ src/GemeindeatlasClient.php
@@ -6,21 +6,36 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GemeindeatlasClient
 {
-    private const string API_ENDPOINT = 'https://gemeindeatlas.hartmann-medienproduktion.de/api/';
+    private const string API_URL = 'https://gemeindeatlas.hartmann-medienproduktion.de/api/';
+    
     public function __construct(
         private readonly HttpClientInterface $httpClient,
     ){}
 
-    public function findAllByName(string $name, ?MunicipalityLevelEnum $level = null, int $page = 1): ?array
+    public function findAll(
+        int $page = 1,
+        ?string $name = null,
+        ?MunicipalityLevelEnum $level = null,
+        ?array $regionalkeys = null,
+        ?int $population_greater_than = null,
+        ?int $population_less_than = null,
+    ): ?array
     {
-        $query_parts = [
-            'page' => $page,
-            'name' => $name
-        ];
+        $query_parts['page'] = $page;
+
+        if($name !== null) $query_parts['name'] = $name;
+
         if($level !== null) $query_parts['level'] = $level->value;
 
+        if($regionalkeys !== null) $query_parts['regionalkeys'] = implode(',', $regionalkeys);
+
+        if($population_greater_than !== null) $query_parts['bevoelkerung_gesamt[gt]'] = $population_greater_than;
+
+        if($population_less_than !== null) $query_parts['bevoelkerung_gesamt[lt]'] = $population_less_than;
+
+
         $response = $this->httpClient
-            ->request('GET', self::API_ENDPOINT . 'municipalities/?' . http_build_query($query_parts))
+            ->request('GET', self::API_URL . 'municipalities/?' . http_build_query($query_parts))
         ;
 
         if ($response->getStatusCode() !== 200) return null;
@@ -43,7 +58,7 @@ class GemeindeatlasClient
     public function getMunicipalityByRegionalKey(string $regionalKey): ?MunicipalityDto
     {
         $response = $this->httpClient
-            ->request('GET', self::API_ENDPOINT . 'municipalities/' . $regionalKey)
+            ->request('GET', self::API_URL . 'municipalities/' . $regionalKey)
         ;
 
         if ($response->getStatusCode() != 200) return null;
@@ -59,6 +74,18 @@ class GemeindeatlasClient
             bevoelkerungGesamt: $response['bevoelkerungGesamt'],
             bevoelkerungDichte: $response['bevoelkerungDichte'],
         );
+    }
+
+    public function getAllRegionalKeys(): ?array
+    {
+        $response = $this->httpClient
+            ->request('GET', self::API_URL . 'all/regionalkeys')
+        ;
+
+        if ($response->getStatusCode() !== 200) return null;
+
+        return $response->toArray()['member'];
+
     }
 
 }
